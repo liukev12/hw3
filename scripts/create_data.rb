9097ce71d6cd3e7006b71c335414e1273dd237c3
Kevin Liu <kevinliu5890@gmail.com>
2024-05-23 19:45:28 +0000

    commit 5

--- scripts/create_data.rb
+++ scripts/create_data.rb
@@ -1,18 +1,28 @@
 Place.destroy_all
 Entry.destroy_all
 
+puts "Destroyed all Places and Entries"
+
 mexico = Place.new
 mexico["name"] = "Mexico City"
 mexico.save
+
+puts "Created Place: #{mexico.name}"
 
 charleston = Place.new
 charleston["name"] = "Charleston"
 charleston.save
 
+puts "Created Place: #{charleston.name}"
+
 beijing = Place.new
 beijing["name"] = "Beijing"
 beijing.save
 
+puts "Created Place: #{beijing.name}"
+
 amsterdam = Place.new
 amsterdam["name"] = "Amsterdam"
 amsterdam.save
+
+puts "Created Place: #{amsterdam.name}"
